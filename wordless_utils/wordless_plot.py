2023f82902e6e5561c2153bb76d04e76793959ed
BLKSerene <blkserene@gmail.com>
2018-10-10 21:02:04 +0800

    Work Area: Update Wordlist/N-gram/Collocation - Plot Settings

--- wordless_utils/wordless_plot.py
+++ wordless_utils/wordless_plot.py
@@ -11,8 +11,8 @@ from matplotlib import pyplot
 from wordless_utils import wordless_misc
 
 def wordless_plot_freq(main, freq_distribution, rank_min, rank_max, use_pct, use_cumulative, label_x):
-    freq_totals = [sum(freqs_file) for freqs_file in zip(*freq_distribution.values())]
-    freq_total = sum(freq_totals)
+    total_freqs = [sum(freqs_file) for freqs_file in zip(*freq_distribution.values())]
+    total_freq = sum(total_freqs)
 
     freq_distribution = sorted(freq_distribution.items(), key = wordless_misc.multi_sorting)
 
@@ -39,9 +39,9 @@ def wordless_plot_freq(main, freq_distribution, rank_min, rank_max, use_pct, use
 
     if use_pct:
         for i, file in enumerate(main.wordless_files.selected_files()):
-            pyplot.plot([freq_files[i] / freq_totals[i] * 100  for freq_files in freqs], label = file['name'])
+            pyplot.plot([freq_files[i] / total_freqs[i] * 100  for freq_files in freqs], label = file['name'])
 
-        pyplot.plot([sum(freq_files) / freq_total * 100 for freq_files in freqs], label = main.tr('Total'))
+        pyplot.plot([sum(freq_files) / total_freq * 100 for freq_files in freqs], label = main.tr('Total'))
     else:
         for i, file in enumerate(main.wordless_files.selected_files()):
             pyplot.plot([freq_files[i] for freq_files in freqs], label = file['name'])
@@ -55,3 +55,22 @@ def wordless_plot_freq(main, freq_distribution, rank_min, rank_max, use_pct, use
 
     pyplot.legend()
     pyplot.show()
+
+def wordless_plot_score(main, score_distribution, rank_min, rank_max, label_x):
+    score_distribution = sorted(score_distribution.items(), key = wordless_misc.multi_sorting)
+
+    collocates = [item[0] for item in score_distribution[rank_min - 1 : rank_max - 1]]
+    scores = [item[1] for item in score_distribution if item[0] in collocates]
+
+    for i, file in enumerate(main.wordless_files.selected_files()):
+        pyplot.plot([scores_files[i] for scores_files in scores], label = file['name'])
+
+    pyplot.xlabel(label_x)
+    pyplot.ylabel(main.tr('Score'))
+    pyplot.xticks(range(len(collocates)), [', '.join(collocate) for collocate in collocates], rotation = 90)
+
+    pyplot.grid(True, color = 'silver')
+
+    pyplot.legend()
+    pyplot.show()
+
