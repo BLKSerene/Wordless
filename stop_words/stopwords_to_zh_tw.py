c3cbd1cd82ad8c28d2d00cc273babe77040842c2
BLKSerene <blkserene@gmail.com>
2018-12-23 20:45:10 +0800

    Settings: Update global settings - languages

--- stop_words/stopwords_to_zh_tw.py
+++ stop_words/stopwords_to_zh_tw.py
@@ -7,19 +7,17 @@
 #
 
 import json
+import os
+import shutil
 
 import pyhanlp
 import spacy.lang.zh
 
-# Stopwords ISO
-with open(r'Stopwords ISO/stopwords_iso.json', 'r', encoding = 'utf_8') as f:
-    stop_words = json.load(f)['zh']
-
-with open(r'Stopwords ISO/stop_words_zh_tw.txt', 'w', encoding = 'utf_8') as f:
-    for stop_word in stop_words:
-        f.write(f'{pyhanlp.HanLP.convertToTraditionalChinese(stop_word)}\n')
-
 # HanLP
+path_pyhanlp = pyhanlp.__path__[0]
+shutil.copy(os.path.join(path_pyhanlp, r'static/data/dictionary/stopwords.txt'),
+	        r'HanLP/stop_words_zh_cn.txt')
+
 with open(r'HanLP/stop_words_zh_cn.txt', 'r', encoding = 'utf_8') as f:
     stop_words = [line.rstrip() for line in f]
 
@@ -31,5 +29,13 @@ with open(r'HanLP/stop_words_zh_tw.txt', 'w', encoding = 'utf_8') as f:
 stop_words = sorted(spacy.lang.zh.STOP_WORDS)
 
 with open(r'spaCy/stop_words_zh_tw.txt', 'w', encoding = 'utf_8') as f:
+    for stop_word in stop_words:
+        f.write(f'{pyhanlp.HanLP.convertToTraditionalChinese(stop_word)}\n')
+
+# Stopwords ISO
+with open(r'Stopwords ISO/stopwords_iso.json', 'r', encoding = 'utf_8') as f:
+    stop_words = json.load(f)['zh']
+
+with open(r'Stopwords ISO/stop_words_zh_tw.txt', 'w', encoding = 'utf_8') as f:
     for stop_word in stop_words:
         f.write(f'{pyhanlp.HanLP.convertToTraditionalChinese(stop_word)}\n')
