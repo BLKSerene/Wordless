de12eed97c6d1db0fbd1449a9a4937a3cb01d60c
BLKSerene <blkserene@gmail.com>
2019-01-02 14:10:24 +0800

    Doc: Update copyright year

--- wordless_tagsets/jpn_unidic.py
+++ wordless_tagsets/jpn_unidic.py
@@ -1,7 +1,7 @@
 #
 # Wordless: Mapping Table (UniDic Tagset -> Universal POS Tags)
 #
-# Copyright (C) 2018 Ye Lei (叶磊) <blkserene@gmail.com>
+# Copyright (C) 2018-2019 Ye Lei (叶磊) <blkserene@gmail.com>
 #
 # License Information: https://github.com/BLKSerene/Wordless/blob/master/LICENSE.txt
 #
