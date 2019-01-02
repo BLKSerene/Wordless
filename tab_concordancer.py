2e344846a826575e64df70be124e1f1049220810
BLKSerene <blkserene@gmail.com>
2019-01-02 14:10:24 +0800

    Doc: Update copyright year

--- tab_concordancer.py
+++ tab_concordancer.py
@@ -1,7 +1,7 @@
 #
 # Wordless: Concordancer
 #
-# Copyright (C) 2018 Ye Lei (叶磊) <blkserene@gmail.com>
+# Copyright (C) 2018-2019 Ye Lei (叶磊) <blkserene@gmail.com>
 #
 # License: https://github.com/BLKSerene/Wordless/blob/master/LICENSE.txt
 #
