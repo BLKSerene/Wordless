ad1a0f7bd9cb046b2b319fa11903efb70e1406fb
BLKSerene <blkserene@gmail.com>
2019-01-13 19:12:53 +0800

    Add pybo's Tibetan tokenizer / POS tagger / lemmatizer

--- wordless_tagsets/all_universal.py
+++ wordless_tagsets/all_universal.py
@@ -12,21 +12,20 @@
 
 mappings = [
     ['ADJ', 'ADJ', 'Adjectives', 'big, old, green, African, incomprehensible\nfirst, second, third'],
-    ['ADV', 'ADV', 'Adverb', 'very, well, exactly, tomorrow, up, down\nwhere, when, how, why\nhere, there, now, then\nsomewhere, sometime, anywhere, anytime\neverywhere, always\nnowhere, never'],
-    ['NOUN', 'NOUN', 'Noun', 'girl, cat, tree, air, beauty'],
-    ['PROPN', 'PROPN', 'Proper noun', 'Mary, John, London, NATO, HBO'],
-    ['PRON', 'PRON', 'Pronoun', 'I, you, he, she it, we, they\nmyself, yourself, himself, herself, itself, ourselves, yourselves, theirselves\nwho, what (What do you think?)\nwho, what (I wonder what you think.)\nsomebody, something, anybody, anything\neverybody, everything\nnobody, nothing\nmine, yours, his, hers, its, ours, theirs'],
-    ['VERB', 'VERB', 'Verb', 'run, eat\nruns, ate\nrunning, eating'],
-    ['AUX', 'AUX', 'Auxiliary', 'has (done), is (doing), will (do)\nwas (done), got (done)\nshould (do), must (do)\nis (He is a teacher.)'],
-
     ['ADP', 'ADP', 'Adposition', 'in, to, during'],
+    ['ADV', 'ADV', 'Adverb', 'very, well, exactly, tomorrow, up, down\nwhere, when, how, why\nhere, there, now, then\nsomewhere, sometime, anywhere, anytime\neverywhere, always\nnowhere, never'],
+    ['AUX', 'AUX', 'Auxiliary', 'has (done), is (doing), will (do)\nwas (done), got (done)\nshould (do), must (do)\nis (He is a teacher.)'],
     ['CONJ', 'CONJ', 'Conjunction', 'and, or, but\nthat (I believe that he will come), if, while'],
     ['CCONJ', 'CCONJ', 'Coordinating conjunction', 'and, or, but'],
     ['SCONJ', 'SCONJ', 'Subordinating conjunction', 'that (I believe that he will come), if, while'],
     ['DET', 'DET', 'Determiner', 'the, a, an\n[Czech] můj, tvůj, jeho, její, náš, váš, jejich; [English] my, your\nthis (I saw this car yesterday)\nwhich (Which car do you like?)\nwhich (I wonder which car you like.)\nany, all, no (We have no cars available.)'],
+    ['INTJ', 'INTJ', 'Interjection', 'psst, ouch, bravo, hello'],
+    ['NOUN', 'NOUN', 'Noun', 'girl, cat, tree, air, beauty'],
+    ['PROPN', 'PROPN', 'Proper noun', 'Mary, John, London, NATO, HBO'],
     ['NUM', 'NUM', 'Numeral', '0, 1, 2, 3, 4, 5, 2014, 1000000, 3.14159265359\none, two, three, seventy-seven\nI, II, III, IV, V, MMXIV'],
     ['PART', 'PART', 'Particle', '[English] ‘s\n[English] not; [German] nicht\n[Japanese] か; [Turkish] mu\n[Czech] ať, kéž, nechť\n[Chinese] 了'],
-    ['INTJ', 'INTJ', 'Interjection', 'psst, ouch, bravo, hello'],
+    ['PRON', 'PRON', 'Pronoun', 'I, you, he, she it, we, they\nmyself, yourself, himself, herself, itself, ourselves, yourselves, theirselves\nwho, what (What do you think?)\nwho, what (I wonder what you think.)\nsomebody, something, anybody, anything\neverybody, everything\nnobody, nothing\nmine, yours, his, hers, its, ours, theirs'],
+    ['VERB', 'VERB', 'Verb', 'run, eat\nruns, ate\nrunning, eating'],
 
     ['PUNCT', 'PUNCT', 'Punctuation', '. , ( )'],
     ['SYM', 'SYM', 'Symbol', '$, %, §, ©\n+, −, ×, ÷, =, <, >\n:), ♥‿♥, 😝\njohn.doe@universal.org, http://universaldependencies.org/, 1-800-COMPANY'],
