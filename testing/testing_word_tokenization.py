37ae725730c1563438dfaf8e8230ff5a487fc68b
BLKSerene <blkserene@gmail.com>
2019-02-09 00:45:34 +0800

    Dependencies: Add lemmalist-greek; Utils: Add lemmalist-greek's Ancient Greek lemma list

--- testing/testing_word_tokenization.py
+++ testing/testing_word_tokenization.py
@@ -14,7 +14,7 @@ import sys
 
 from PyQt5.QtCore import *
 
-sys.path.append('..')
+sys.path.append('.')
 
 from wordless_text import wordless_text, wordless_text_processing
 from wordless_settings import init_settings_default, init_settings_global
