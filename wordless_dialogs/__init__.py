d267fd405d4d540d11a2e27e6408973c1375a377
BLKSerene <blkserene@gmail.com>
2019-02-22 00:01:40 +0800

    Menu: Add Preferences - Settings - General

---
+++ wordless_dialogs/__init__.py
@@ -0,0 +1,20 @@
+#
+# Wordless: Dialogs
+#
+# Copyright (C) 2018-2019  Ye Lei (叶磊)
+#
+# This source file is licensed under GNU GPLv3.
+# For details, see: https://github.com/BLKSerene/Wordless/blob/master/LICENSE.txt
+#
+# All other rights reserved.
+#
+
+__all__ = [
+    'wordless_dialog',
+    'wordless_dialog_context_settings',
+    'wordless_dialog_filter_results',
+    'wordless_dialog_help',
+    'wordless_dialog_misc',
+    'wordless_dialog_search_results',
+    'wordless_message_box'
+]
