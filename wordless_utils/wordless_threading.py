03de785033675b88ded743bbd4b045f7b67fbadb
BLKSerene <blkserene@gmail.com>
2019-02-25 15:33:16 +0800

    Work Area: Show hint messages when searching in results

--- wordless_utils/wordless_threading.py
+++ wordless_utils/wordless_threading.py
@@ -13,22 +13,37 @@ from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 
-class Worker_Process_Data(QObject):
-    progress_updated = pyqtSignal(str)
-
-    def __init__(self, main, dialog_processing, data_received):
+class Wordless_Worker(QObject):
+    def __init__(self, main):
         super().__init__()
 
         self.main = main
 
-        self.progress_updated.connect(dialog_processing.update_progress)
+class Wordless_Worker_Process_Data(Wordless_Worker):
+    progress_updated = pyqtSignal(str)
+
+    def __init__(self, main, dialog_progress, data_received):
+        super().__init__(main)
+
+        self.progress_updated.connect(dialog_progress.update_progress)
         self.processing_finished.connect(data_received)
 
-class Thread_Process_Data(QThread):
-    def __init__(self, worker_process_data):
+class Wordless_Thread(QThread):
+    def __init__(self, worker):
         super().__init__()
 
-        worker_process_data.moveToThread(self)
+        worker.moveToThread(self)
+
+        self.finished.connect(worker.deleteLater)
+
+class Wordless_Thread_Process_Data(Wordless_Thread):
+    def __init__(self, worker_process_data):
+        super().__init__(worker_process_data)
 
         self.started.connect(worker_process_data.process_data)
-        self.finished.connect(worker_process_data.deleteLater)
+
+class Wordless_Thread_Search_Results(Wordless_Thread):
+    def __init__(self, worker_search_results):
+        super().__init__(worker_search_results)
+
+        self.started.connect(worker_search_results.search_results)
