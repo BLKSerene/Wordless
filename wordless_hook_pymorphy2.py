0923bf6c0b69203f86de3f83039531fab2cb1b53
BLKSerene <blkserene@gmail.com>
2019-03-03 14:33:18 +0800

    Release: Add packaging script, spec file, and runtime hook for pymorphy2

---
+++ wordless_hook_pymorphy2.py
@@ -0,0 +1,29 @@
+#
+# Wordless: Building Hook for pymorphy2
+#
+# Copyright (C) 2018-2019  Ye Lei (叶磊)
+#
+# This source file is licensed under GNU GPLv3.
+# For details, see: https://github.com/BLKSerene/Wordless/blob/master/LICENSE.txt
+#
+# All other rights reserved.
+#
+
+# Reference: https://github.com/pyinstaller/pyinstaller/issues/3050
+
+import pkg_resources
+
+def iter_entry_points(group, name = None):
+    for entry_point in entry_points:
+    	if entry_point.find(group) > -1:
+	        entry_point_parsed = pkg_resources.EntryPoint.parse(entry_point)
+	        entry_point_parsed.dist = pkg_resources.Distribution()
+
+	        yield entry_point_parsed
+
+entry_points = [
+    'uk = pymorphy2_dicts_uk',
+    'ru = pymorphy2_dicts_ru'
+]
+
+pkg_resources.iter_entry_points = iter_entry_points
