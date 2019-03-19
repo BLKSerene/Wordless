ddc1c2b9f2e67a743b09e0f359af2b87cbaa6389
BLKSerene <blkserene@gmail.com>
2019-03-19 17:05:53 +0800

    Main Window: Update layout on Linux

--- src/wordless_figs/wordless_fig.py
+++ src/wordless_figs/wordless_fig.py
@@ -15,7 +15,7 @@ import matplotlib
 import matplotlib.pyplot
 
 def show_fig():
-    if platform.system() == 'Windows':
+    if platform.system() in ['Windows', 'Linux']:
         matplotlib.pyplot.get_current_fig_manager().window.showMaximized()
     # Do not maximize the window to avoid segfault on macOS
     elif platform.system() == 'Darwin':
