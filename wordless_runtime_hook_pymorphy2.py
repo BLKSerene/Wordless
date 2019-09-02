d9fb9312d998a11b9d937b85a27a67f0094d71ee
BLKSerene <blkserene@gmail.com>
2019-09-02 12:02:36 +0800

    Releases: Update runtime hook for pymorphy2

--- wordless_runtime_hook_pymorphy2.py
+++ wordless_runtime_hook_pymorphy2.py
@@ -1,5 +1,5 @@
 #
-# Wordless: Packaging Hook - pymorphy2
+# Wordless: Runtime Hook - pymorphy2
 #
 # Copyright (C) 2018-2019  Ye Lei (叶磊)
 #
@@ -14,15 +14,15 @@ import pkg_resources
 # Reference: https://github.com/pyinstaller/pyinstaller/issues/3050
 def iter_entry_points(group, name = None):
     for entry_point in entry_points:
-    	if entry_point.find(group) > -1:
-	        entry_point_parsed = pkg_resources.EntryPoint.parse(entry_point)
-	        entry_point_parsed.dist = pkg_resources.Distribution()
+        if entry_point.find(group) > -1:
+            entry_point_parsed = pkg_resources.EntryPoint.parse(entry_point)
+            entry_point_parsed.dist = pkg_resources.Distribution()
 
-	        yield entry_point_parsed
+            yield entry_point_parsed
 
 entry_points = [
-    'uk = pymorphy2_dicts_uk',
-    'ru = pymorphy2_dicts_ru'
+    'ru = pymorphy2_dicts_ru',
+    'uk = pymorphy2_dicts_uk'
 ]
 
 pkg_resources.iter_entry_points = iter_entry_points
