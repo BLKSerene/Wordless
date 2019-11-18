afe397447251379b6b67e3797cc2bf317d53ad9e
BLKSerene <blkserene@gmail.com>
2019-11-18 19:18:14 +0800

    Dependencies: Upgrade SciPy to 1.3.2

--- src/wordless_dialogs/wordless_dialog_help.py
+++ src/wordless_dialogs/wordless_dialog_help.py
@@ -327,7 +327,7 @@ class Wordless_Dialog_Acks(wordless_dialog.Wordless_Dialog_Info):
             # SciPy
             [
                 'SciPy', 'https://www.scipy.org',
-                '1.3.1',
+                '1.3.2',
                 'SciPy Developers',
                 'BSD-3-Clause', 'https://www.scipy.org/scipylib/license.html'
             ],
