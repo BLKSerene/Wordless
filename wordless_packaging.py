ca04e33d37dc3ee978dddadc743504cfd3162b0b
BLKSerene <blkserene@gmail.com>
2019-11-24 20:47:10 +0800

    Releases: Update packaging script

--- wordless_packaging.py
+++ wordless_packaging.py
@@ -1,5 +1,5 @@
 #
-# Wordless: Packaging
+# Wordless: Packaging - Packaging Script
 #
 # Copyright (C) 2018-2019  Ye Lei (叶磊)
 #
@@ -16,65 +16,69 @@ import shutil
 import subprocess
 import time
 
+def print_with_elapsed_time(message):
+    print(f'[{datetime.timedelta(seconds = round(time.time() - time_start))}] {message}')
+
 time_start = time.time()
 
 # Package
-print(f'[{datetime.timedelta(seconds = round(time.time() - time_start))}] Packaging Wordless ...')
+print_with_elapsed_time('Start packaging ...')
 
 if platform.system() == 'Windows':
-    os.system('python -m PyInstaller -y wordless_packaging.spec')
+    return_val_packaging = subprocess.call('pyinstaller --noconfirm wordless_packaging.spec', shell = True)
 elif platform.system() == 'Darwin':
-    subprocess.call([
-        'python3',
-        '-m',
-        'PyInstaller',
-        '-y',
-        'wordless_packaging.spec'
-    ])
+    return_val_packaging = subprocess.call('python3 -m PyInstaller --noconfirm wordless_packaging.spec', shell = True)
 elif platform.system() == 'Linux':
-    os.system('python3.7 -m PyInstaller -y wordless_packaging.spec')
+    subprocess.call('python3.7 -m PyInstaller --noconfirm wordless_packaging.spec', shell = True)
 
-print(f'[{datetime.timedelta(seconds = round(time.time() - time_start))}] Packaging completed successfully!')
+if return_val_packaging == 0:
+    print_with_elapsed_time('Packaging done!')
 
-os.chdir('dist/Wordless')
+    # Create folders
+    print_with_elapsed_time('Creating folders ...')
 
-# Create folders
-if not os.path.exists('Import') or not os.path.exists('Export'):
-    print(f'[{datetime.timedelta(seconds = round(time.time() - time_start))}] Creating folders ...')
+    if not os.path.exists('dist/Wordless/Import'):
+        os.mkdir('dist/Wordless/Import')
+    if not os.path.exists('dist/Wordless/Export'):
+        os.mkdir('dist/Wordless/Export')
 
-    if not os.path.exists('Import'):
-        os.mkdir('Import')
-    if not os.path.exists('Export'):
-        os.mkdir('Export')
+    # Copy files
+    if platform.system() == 'Darwin':
+        for dir_src, dirs, files in os.walk('dist/Wordless/'):
+            dir_src = os.path.realpath(dir_src)
+            dir_app = dir_src.replace('dist/Wordless', 'dist/Wordless.app/Contents/MacOS')
 
-# Copy files
-if platform.system() == 'Darwin':
-    for dir_src, dirs, files in os.walk('.'):
-        dir_src = os.path.realpath(dir_src)
-        dir_app = dir_src.replace('dist/Wordless', 'dist/Wordless.app/Contents/MacOS')
+            print_with_elapsed_time(f'Copying folder {dir_app} ...')
 
-        print(f'[{datetime.timedelta(seconds = round(time.time() - time_start))}] Copying folder {dir_app} ...')
+            if not os.path.exists(dir_app):
+                os.mkdir(dir_app)
 
-        if not os.path.exists(dir_app):
-            os.mkdir(dir_app)
+            for file in files:
+                path_src = os.path.join(dir_src, file)
+                path_app = os.path.join(dir_app, file)
 
-        for file in files:
-            path_src = os.path.join(dir_src, file)
-            path_app = os.path.join(dir_app, file)
+                if not os.path.exists(path_app):
+                    shutil.copy(path_src, path_app)
 
-            if not os.path.exists(path_app):
-                shutil.copy(path_src, path_app)
+        print_with_elapsed_time(f'Finished copying all files!')
 
-    print(f'[{datetime.timedelta(seconds = round(time.time() - time_start))}] Finished copying all files!')
+    # Test
+    print_with_elapsed_time(f'Testing ...')
 
-# Testing
-print(f'[{datetime.timedelta(seconds = round(time.time() - time_start))}] Running Wordless ...')
+    if platform.system() == 'Windows':
+        os.chdir('dist/Wordless')
 
-if platform.system() == 'Windows':
-    os.system('start Wordless.exe')
-elif platform.system() == 'Darwin':
-    os.chdir('..')
+        return_val_test = subprocess.call(os.path.join(os.getcwd(), 'Wordless.exe'), shell = True)
+    elif platform.system() == 'Darwin':
+        return_val_test = subprocess.call(os.path.join(os.getcwd(), 'Wordless.app/Contents/Macos/Wordless'), shell = True)
+    elif platform.system() == 'Linux':
+        os.chdir('dist/Wordless')
 
-    subprocess.call(['open', './Wordless.app'])
-elif platform.system() == 'Linux':
-    os.system('./Wordless')
+        return_val_test = subprocess.call('./Wordless', shell = True)
+
+    if return_val_test == 0:
+        print_with_elapsed_time(f'Pass!')
+    else:
+        print_with_elapsed_time(f'Fail!')
+else:
+    print_with_elapsed_time(f'Packaging failed!')
