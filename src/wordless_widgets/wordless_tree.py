ce3e769432e171799e0c9fc912ec1a9b29ca2d36
BLKSerene <blkserene@gmail.com>
2020-06-05 20:31:28 +0800

    Doc: Update copyright year

--- src/wordless_widgets/wordless_tree.py
+++ src/wordless_widgets/wordless_tree.py
@@ -1,7 +1,7 @@
 #
 # Wordless: Widgets - Tree
 #
-# Copyright (C) 2018-2019  Ye Lei (叶磊)
+# Copyright (C) 2018-2020  Ye Lei (叶磊)
 #
 # This source file is licensed under GNU GPLv3.
 # For details, see: https://github.com/BLKSerene/Wordless/blob/master/LICENSE.txt
