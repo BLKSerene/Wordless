d2b714399d2da593ca225dda455ac691ce5d66cc
BLKSerene <blkserene@gmail.com>
2020-06-05 20:31:28 +0800

    Doc: Update copyright year

--- src/wordless_measures/wordless_measures_statistical_significance.py
+++ src/wordless_measures/wordless_measures_statistical_significance.py
@@ -1,7 +1,7 @@
 #
 # Wordless: Measures - Statistical Significance
 #
-# Copyright (C) 2018-2019  Ye Lei (叶磊)
+# Copyright (C) 2018-2020  Ye Lei (叶磊)
 #
 # This source file is licensed under GNU GPLv3.
 # For details, see: https://github.com/BLKSerene/Wordless/blob/master/LICENSE.txt
