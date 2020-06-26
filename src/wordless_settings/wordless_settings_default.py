5ba0453a72e3b800f83c97be04139672ff37098a
BLKSerene <blkserene@gmail.com>
2020-06-26 19:07:34 +0800

    Utils: Update PyThaiNLP's Thai word tokenizers

--- src/wordless_settings/wordless_settings_default.py
+++ src/wordless_settings/wordless_settings_default.py
@@ -1159,7 +1159,7 @@ def init_settings_default(main):
                 'tam': main.tr('spaCy - Tamil Word Tokenizer'),
                 'tat': main.tr('spaCy - Tatar Word Tokenizer'),
                 'tel': main.tr('spaCy - Telugu Word Tokenizer'),
-                'tha': main.tr('PyThaiNLP - Maximum Matching Algorithm + TCC'),
+                'tha': main.tr('PyThaiNLP - Maximum Matching + TCC'),
                 'bod': main.tr('botok - Tibetan Word Tokenizer'),
                 'tur': main.tr('spaCy - Turkish Word Tokenizer'),
                 'ukr': main.tr('spaCy - Ukrainian Word Tokenizer'),
