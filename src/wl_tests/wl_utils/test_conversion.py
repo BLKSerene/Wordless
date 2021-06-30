9a5ab763eb1bf27583652426f330bc0372456dda
BLKSerene <blkserene@gmail.com>
2021-07-01 00:09:29 +0800

    Utils: Update NLTK's word tokenizers

--- src/wl_tests/wl_utils/test_conversion.py
+++ src/wl_tests/wl_utils/test_conversion.py
@@ -21,24 +21,31 @@ from wl_utils import wl_conversion
 
 main = wl_test_init.Wl_Test_Main()
 
+settings_langs = main.settings_global['langs']
+settings_file_encodings = main.settings_global['file_encodings']
+
 to_lang_text = {
     lang_code_639_3: lang_text
-    for lang_text, (lang_code_639_3, _, _) in main.settings_global['langs'].items()
+    for lang_text, (lang_code_639_3, _, _) in settings_langs.items()
 }
 to_iso_639_1 = {
     lang_code_639_3: lang_code_639_1
-    for _, (lang_code_639_3, lang_code_639_1, _) in main.settings_global['langs'].items()
+    for lang_code_639_3, lang_code_639_1, _ in settings_langs.values()
 }
 to_iso_639_3 = {
     lang_code_639_1: lang_code_639_3
-    for _, (lang_code_639_3, lang_code_639_1, _) in main.settings_global['langs'].items()
+    for lang_code_639_3, lang_code_639_1, _ in settings_langs.values()
+}
+get_lang_family = {
+    lang_code_639_3: lang_family
+    for lang_code_639_3, _, lang_family in settings_langs.values()
 }
 
-@pytest.mark.parametrize('lang_text', main.settings_global['langs'].keys())
+@pytest.mark.parametrize('lang_text', settings_langs.keys())
 def test_to_lang_code(lang_text):
     lang_code = wl_conversion.to_lang_code(main, lang_text)
 
-    assert lang_code == main.settings_global['langs'][lang_text][0]
+    assert lang_code == settings_langs[lang_text][0]
 
 @pytest.mark.parametrize('lang_code', to_lang_text.keys())
 def test_to_lang_text(lang_code):
@@ -58,19 +65,25 @@ def test_to_iso_639_3(lang_code):
 
     assert lang_code_639_3 == to_iso_639_3[lang_code]
 
-@pytest.mark.parametrize('encoding_text', main.settings_global['file_encodings'])
+@pytest.mark.parametrize('lang_code', to_iso_639_1.keys())
+def test_get_lang_family(lang_code):
+    lang_family = wl_conversion.get_lang_family(main, lang_code)
+
+    assert lang_family == get_lang_family[lang_code]
+
+@pytest.mark.parametrize('encoding_text', settings_file_encodings)
 def test_to_encoding_code(encoding_text):
     len_encoding_text = max([len(encoding_text)
-                             for encoding_text in main.settings_global['file_encodings']])
+                             for encoding_text in settings_file_encodings])
     encoding_code = wl_conversion.to_encoding_code(main, encoding_text)
 
-    assert encoding_code == main.settings_global['file_encodings'][encoding_text]
+    assert encoding_code == settings_file_encodings[encoding_text]
 
-@pytest.mark.parametrize('encoding_code', main.settings_global['file_encodings'].values())
+@pytest.mark.parametrize('encoding_code', settings_file_encodings.values())
 def test_to_encoding_text(encoding_code):
     len_encoding_code = max([len(encoding_code)
-                             for encoding_code in main.settings_global['file_encodings'].values()])
+                             for encoding_code in settings_file_encodings.values()])
     encoding_text = wl_conversion.to_encoding_text(main, encoding_code)
 
     assert encoding_text == {encoding_code: encoding_text
-                             for encoding_text, encoding_code in main.settings_global['file_encodings'].items()}[encoding_code]
+                             for encoding_text, encoding_code in settings_file_encodings.items()}[encoding_code]
