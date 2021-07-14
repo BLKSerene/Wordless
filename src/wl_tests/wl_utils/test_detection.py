3359577b344b911537eb1dd68368abd8db76b5b8
BLKSerene <blkserene@gmail.com>
2021-07-14 21:13:44 +0800

    Dependencies: 1. Add Charset Normalizer 2. Remove cChardet and chardet 3. Upgrade Requests to 2.26.0; Utils: Update encoding detection

--- src/wl_tests/wl_utils/test_detection.py
+++ src/wl_tests/wl_utils/test_detection.py
@@ -9,6 +9,7 @@
 # All other rights reserved.
 #
 
+import glob
 import os
 import re
 import sys
@@ -24,31 +25,40 @@ main = wl_test_init.Wl_Test_Main()
 main.settings_custom['auto_detection']['detection_settings']['number_lines_no_limit'] = True
 
 # Encoding detection
-@pytest.mark.parametrize('file_name', os.listdir(f'wl_tests/files/wl_utils/wl_detection/encoding/'))
-def test_detection_encoding(file_name):
-    file = {}
+@pytest.mark.parametrize('file_path', glob.glob(f'wl_tests/files/wl_utils/wl_detection/encoding/*.txt'))
+def test_detection_encoding(file_path):
+    file_name = os.path.basename(file_path)
 
-    file['path'] = f'wl_tests/files/wl_utils/wl_detection/encoding/{file_name}'
-    file['name'] = os.path.basename(file['path'])
-    file['encoding'] = 'utf_8'
+    print(f'Detecting encoding for file "{file_name}"... ', end = '')
 
-    encoding_code = wl_detection.detect_encoding(main, file["path"])
-
+    encoding_code = wl_detection.detect_encoding(main, file_path)
     encoding_code_file = re.search(r'(?<=\()[^\(\)]+?(?=\)\.txt)', file_name).group()
-    encoding_code_file = encoding_code_file.lower()
-    encoding_code_file = encoding_code_file.replace('-', '_')
+
+    print(encoding_code)
 
     assert encoding_code == encoding_code_file
 
 # Language detection
-@pytest.mark.parametrize('file_name', os.listdir(f'wl_tests/files/wl_utils/wl_detection/lang/'))
-def test_detection_lang(file_name):
+@pytest.mark.parametrize('file_path', glob.glob(f'wl_tests/files/wl_utils/wl_detection/lang/*.txt'))
+def test_detection_lang(file_path):
     file = {}
 
-    file['path'] = f'wl_tests/files/wl_utils/wl_detection/lang/{file_name}'
-    file['name'] = os.path.basename(file['path'])
+    file['path'] = file_path
     file['encoding'] = 'utf_8'
+
+    file_name = os.path.basename(file_path)
+
+    print(f'Detecting language for file "{file_name}"... ', end = '')
 
     lang_code = wl_detection.detect_lang(main, file)
 
+    print(lang_code)
+
     assert lang_code == file_name.replace('.txt', '')
+
+if __name__ == '__main__':
+    for file in glob.glob('wl_tests/files/wl_utils/wl_detection/encoding/*.txt'):
+        test_detection_encoding(file)
+
+    for file in glob.glob('wl_tests/files/wl_utils/wl_detection/lang/*.txt'):
+        test_detection_lang(file)
