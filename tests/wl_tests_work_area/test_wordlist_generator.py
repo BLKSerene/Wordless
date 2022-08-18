aa57d9ebc04859c72faf9194023301596322c926
BLKSerene <blkserene@gmail.com>
2022-08-18 20:25:14 +0800

    Work Area: Remove Concordancer / Parallel Concordancer - Generation Settings - Sampling Method

--- tests/wl_tests_work_area/test_wordlist_generator.py
+++ tests/wl_tests_work_area/test_wordlist_generator.py
@@ -1,5 +1,5 @@
 # ----------------------------------------------------------------------
-# Wordless: Tests - Wordlist Generator
+# Wordless: Tests - Work Area - Wordlist Generator
 # Copyright (C) 2018-2022  Ye Lei (叶磊)
 #
 # This program is free software: you can redistribute it and/or modify
