33e77a6a67615582aec7427c14f43cc6d0e109dc
BLKSerene <blkserene@gmail.com>
2023-08-03 21:54:11 +0800

    Measures: Add readability - Wheeler & Smith's Readability Formula; Work Area: Add Profiler - Readability - Wheeler & Smith's Readability Formula

--- wordless/wl_measures/wl_measures_readability.py
+++ wordless/wl_measures/wl_measures_readability.py
@@ -24,7 +24,7 @@ import re
 import numpy
 
 from wordless.wl_checks import wl_checks_tokens
-from wordless.wl_nlp import wl_pos_tagging, wl_syl_tokenization
+from wordless.wl_nlp import wl_pos_tagging, wl_sentence_tokenization, wl_syl_tokenization
 from wordless.wl_utils import wl_misc, wl_paths
 
 def get_counts(main, text):
@@ -96,17 +96,17 @@ def get_count_words_letters(words, len_min = 1, len_max = None):
 
 def get_count_words_syls(syls_words, len_min = 1, len_max = None):
     if len_max:
-        return len([
-            True
+        return sum((
+            1
             for syls in syls_words
             if len_min <= len(syls) <= len_max
-        ])
+        ))
     else:
-        return len([
-            True
+        return sum((
+            1
             for syls in syls_words
             if len(syls) >= len_min
-        ])
+        ))
 
 def get_count_words_outside_wordlist(words, wordlist):
     easy_words = set()
@@ -1009,12 +1009,120 @@ def trankle_bailers_readability_formula(main, text):
 
     return trankle_bailers
 
+# Wheeler & Smith's Readability Formula
+# Reference: Wheeler, L. R., & Smith, E. H. (1954). A practical readability formula for the classroom teacher in the primary grades. Elementary English, 31(7), 397–399.
+UNIT_TERMINATORS = ''.join([
+    # Sentence terminators plus colons and semicolons
+    '\u0021', '\u002E', '\u003A', '\u003B', '\u003F',
+    '\u037E',
+    '\u0589',
+    '\u061B', '\u061D', '\u061E', '\u061F', '\u06D4',
+    '\u0700', '\u0701', '\u0702', '\u0703', '\u0704', '\u0705', '\u0706', '\u0707', '\u0708', '\u0709',
+    '\u07F9',
+    '\u0837', '\u0839', '\u083D', '\u083E',
+    '\u0964', '\u0965',
+    '\u104A', '\u104B',
+    '\u1362', '\u1364', '\u1365', '\u1366', '\u1367', '\u1368',
+    '\u166E',
+    '\u1735', '\u1736',
+    '\u17D4', '\u17D5',
+    '\u1803', '\u1804', '\u1809',
+    '\u1944', '\u1945',
+    '\u1AA8', '\u1AA9', '\u1AAA', '\u1AAB',
+    '\u1B5A', '\u1B5B', '\u1B5E', '\u1B5F', '\u1B7D', '\u1B7E',
+    '\u1C3B', '\u1C3C',
+    '\u1C7E', '\u1C7F',
+    '\u203C', '\u2047', '\u2048', '\u2049', '\u203D',
+    '\u2E2E', '\u2E53', '\u2E54', '\u2E3C',
+    '\u3002',
+    '\uA4FF',
+    '\uA60E', '\uA60F',
+    '\uA6F3', '\uA6F4', '\uA6F6', '\uA6F7',
+    '\uA876', '\uA877',
+    '\uA8CE', '\uA8CF',
+    '\uA92F',
+    '\uA9C8', '\uA9C9',
+    '\uAA5D', '\uAA5E', '\uAA5F',
+    '\uAAF0', '\uAAF1', '\uABEB',
+    '\uFE52', '\uFE54', '\uFE55', '\uFE56', '\uFE57',
+    '\uFF01', '\uFF0E', '\uFF1A', '\uFF1B', '\uFF1F', '\uFF61',
+    '\U00010857',
+    '\U00010A56', '\U00010A57',
+    '\U00010B99', '\U00010B9A',
+    '\U00010F55', '\U00010F56', '\U00010F57', '\U00010F58', '\U00010F59',
+    '\U00010F86', '\U00010F87', '\U00010F88', '\U00010F89',
+    '\U00011047', '\U00011048',
+    '\U000110BE', '\U000110BF', '\U000110C0', '\U000110C1',
+    '\U00011141', '\U00011142', '\U00011143',
+    '\U000111C5', '\U000111C6', '\U000111CD', '\U000111DE', '\U000111DF',
+    '\U00011238', '\U00011239', '\U0001123B', '\U0001123C',
+    '\U000112A9',
+    '\U0001144B', '\U0001144C',
+    '\U000115C2', '\U000115C3', '\U000115C9', '\U000115CA', '\U000115CB', '\U000115CC', '\U000115CD', '\U000115CE', '\U000115CF', '\U000115D0', '\U000115D1', '\U000115D2', '\U000115D3', '\U000115D4', '\U000115D5', '\U000115D6', '\U000115D7',
+    '\U00011641', '\U00011642',
+    '\U0001173C', '\U0001173D', '\U0001173E',
+    '\U00011944', '\U00011946',
+    '\U00011A42', '\U00011A43',
+    '\U00011A9B', '\U00011A9C',
+    '\U00011C41', '\U00011C42',
+    '\U00011EF7', '\U00011EF8',
+    '\U00011F43', '\U00011F44',
+    '\U00012471', '\U00012472', '\U00012473', '\U00012474',
+    '\U00016A6E', '\U00016A6F',
+    '\U00016AF5',
+    '\U00016B37', '\U00016B38', '\U00016B44',
+    '\U00016E98',
+    '\U0001BC9F',
+    '\U0001DA88', '\U0001DA89', '\U0001DA8A',
+
+    # Dashes: https://util.unicode.org/UnicodeJsps/list-unicodeset.jsp?a=[:Dash%CE%B2=Yes:]
+    '\\\u002D', # The hyphen character needs to be escaped in RegEx square brackets
+    '\u058A',
+    '\u05BE',
+    '\u1400',
+    '\u1806',
+    '\u2010', '\u2011', '\u2012', '\u2013', '\u2014', '\u2015', '\u2053',
+    '\u207B', '\u208B',
+    '\u2212',
+    '\u2E17', '\u2E1A', '\u2E3A', '\u2E3B', '\u2E40', '\u2E5D',
+    '\u301C', '\u3030',
+    '\u30A0',
+    '\uFE31', '\uFE32',
+    '\uFE58', '\uFE63',
+    '\uFF0D',
+    '\U00010EAD'
+])
+
+def wheeler_smiths_readability_formula(main, text):
+    if text.lang in main.settings_global['syl_tokenizers']:
+        text = get_counts(main, text)
+
+        if text.count_words:
+            count_units = len(wl_sentence_tokenization.wl_sentence_seg_split(
+                main,
+                text = ' '.join(wl_misc.flatten_list(text.tokens_multilevel_with_puncs)),
+                terminators = UNIT_TERMINATORS
+            ))
+            count_words_2_syls = get_count_words_syls(text.syls_words, len_min = 2)
+
+            wheeler_smith = (
+                (text.count_words / count_units)
+                * (count_words_2_syls / text.count_words)
+                * 10
+            )
+        else:
+            wheeler_smith = 'text_too_short'
+    else:
+        wheeler_smith = 'no_support'
+
+    return wheeler_smith
+
 # Wiener Sachtextformel
 # References:
 #     Bamberger, R., & Vanecek, E. (1984). Lesen – Verstehen – Lernen – Schreiben. Jugend und Volk.
 #     Lesbarkeitsindex. (2022, July 21). In Wikipedia. https://de.wikipedia.org/w/index.php?title=Lesbarkeitsindex&oldid=224664667
 def wstf(main, text):
-    if text.lang.startswith('deu_') and text.lang in main.settings_global['syl_tokenizers']:
+    if text.lang.startswith('deu_'):
         text = get_counts(main, text)
 
         if text.count_words and text.count_sentences:
