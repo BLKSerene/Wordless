7982becb63763fab50bcdd05160e1f4d10d5b354
BLKSerene <blkserene@gmail.com>
2024-05-24 18:27:24 +0800

    Dependencies: Upgrade Beautiful Soup to 4.12.3

--- utils/wl_generate_acks.py
+++ utils/wl_generate_acks.py
@@ -36,7 +36,7 @@ COL_HEADERS = [
 
 # SPDX License List: https://spdx.org/licenses/
 ACKS = [
-    ['Beautiful Soup', 'https://www.crummy.com/software/BeautifulSoup/', '4.12.2', 'Leonard Richardson', 'MIT', 'https://git.launchpad.net/beautifulsoup/tree/LICENSE'],
+    ['Beautiful Soup', 'https://www.crummy.com/software/BeautifulSoup/', '4.12.3', 'Leonard Richardson', 'MIT', 'https://git.launchpad.net/beautifulsoup/tree/LICENSE'],
     ['Botok', 'https://github.com/OpenPecha/Botok', '0.8.12', 'Hélios Drupchen Hildt', 'Apache-2.0', 'https://github.com/OpenPecha/Botok/blob/master/LICENSE'],
     ['Charset Normalizer', 'https://github.com/Ousret/charset_normalizer', '3.3.2', 'TAHRI Ahmed R.', 'MIT', 'https://github.com/Ousret/charset_normalizer/blob/master/LICENSE'],
     ['khmer-nltk', 'https://github.com/VietHoang1512/khmer-nltk', '1.6', 'Phan Viet Hoang', 'Apache-2.0', 'https://github.com/VietHoang1512/khmer-nltk/blob/main/LICENSE'],
